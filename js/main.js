9f3cf208b4e246098b18d21045eec495c862969f
Blaise Sebagabo <bsebagabo1@studentmail.nait.ca>
2020-01-30 17:56:28 -0700

    added an email confirmation integration with Zapier

--- js/main.js
+++ js/main.js
@@ -14,6 +14,7 @@ firebase.initializeApp(firebaseConfig);
 var registrationsRef = firebase.database().ref('registrations');
 // retrieving data from Firebase
 registrationsRef.on('value', gotData, errorData);
+
 let listOfRegistrations = [];
 document.getElementById('registration-form').addEventListener('submit', submitForm);
 
@@ -65,6 +66,7 @@ function saveRegistration(firstname, lastname, program, major, email, phone, ski
     });
 
 }
+
 
 function gotData(data) {
     try {
